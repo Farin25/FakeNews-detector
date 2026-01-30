c15a0642644c215ebe21d41b2266e7244cc879f3
F4rin <204501139+Farin25@users.noreply.github.com>
2026-01-30 22:50:20 +0100

    Remove old version files: index.html, script.js, and style.css

--- old/V1/script.js
+++
@@ -1,7 +0,0 @@
-/* Alte Datei */
-  const glow = document.getElementById("cursor-glow");
-
-  document.addEventListener("mousemove", (e) => {
-    glow.style.left = e.clientX + "px";
-    glow.style.top = e.clientY + "px";
-  });
